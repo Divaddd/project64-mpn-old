8a306ffded9d54e0287361bec1a8cab76edba4a4
Divaddd <43974972+Divaddd@users.noreply.github.com>
2019-07-24 22:32:51 -0400

    Add files via upload

---
+++ UpdatePlugin.bat
@@ -0,0 +1,12 @@
+@echo off
+set /p answer=This will reset your graphics settings back to default. Type "yes" to proceed or "no" to exit.
+if /I "%answer%"=="yes" goto UpdatePlugin
+if /I "%answer%"=="y" goto UpdatePlugin
+if /I "%answer%"=="no" exit
+if /I "%answer%"=="n" exit
+
+:UpdatePlugin
+TASKKILL /IM Project64-MPN.exe /F
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Divaddd/project64-mpn/master/Plugin/GFX/GLideN64.dll', 'Plugin/GFX/GLideN64.dll')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Divaddd/project64-mpn/master/Plugin/GFX/GLideN64.ini', 'Plugin/GFX/GLideN64.ini')"
+pause
